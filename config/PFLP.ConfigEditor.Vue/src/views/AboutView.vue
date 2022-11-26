9a65a408b5298353306bc84a29e4e72253255847
LazuliKao <lazulikao@nit.zju.edu.cn>
2022-11-26 21:18:27 +0800

    sync

---
+++ config/PFLP.ConfigEditor.Vue/src/views/AboutView.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="about">
+    <h1>PFLP.ConfigEditor.Vue</h1>
+    <ul dir="auto">
+      <h3 dir="auto">一款专门为PixelFaramitaLuminousPolymerization插件设计的配置工具</h3>
+      <h4 dir="auto">配置插件从未如此方便</h4>
+      <li>
+        <h3 dir="auto">
+          <a href="https://github.com/LazuliKao/PixelFaramitaLuminousPolymerizationRes/tree/main/config/PFLP.ConfigEditor.Vue"
+            rel="nofollow">源码</a>（基于Vue.js + TypeScript）
+        </h3>
+      </li>
+    </ul>
+    <h1>PixelFaramitaLuminousPolymerization</h1>
+    <ul dir="auto">
+      <h3 dir="auto">一款功能很基础的基础插件</h3>
+      <h4 dir="auto">上次看到有这么基础的插件还在上次</h4>
+      <li>
+        <h3 dir="auto">
+          <a href="https://gxh.lanzoum.com/b03v3gxbi" rel="nofollow">最新构建</a> 密码：114514
+        </h3>
+      </li>
+      <li>
+        <h3 dir="auto">
+          <a href="/LazuliKao/PixelFaramitaLuminousPolymerizationRes/blob/main/docs/InstallGuide.md">安装教程</a>
+        </h3>
+      </li>
+      <li>
+        <h3 dir="auto">
+          <a href="/LazuliKao/PixelFaramitaLuminousPolymerizationRes/blob/main/docs/RemoteCall.md">接口文档</a>
+        </h3>
+      </li>
+      <li>
+        <h3 dir="auto">
+          <a href="/LazuliKao/PixelFaramitaLuminousPolymerizationRes/blob/main/lang">语言文件</a>
+        </h3>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    vertical-align: top;
+  }
+
+  .h1 {
+    text-align: center;
+  }
+}
+</style>
---
+++ config/PFLP.ConfigEditor.Vue/vite.config.ts
@@ -0,0 +1,28 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: [
+        'vue',
+        {
+          'naive-ui': [
+            'useDialog',
+            'useMessage',
+            'useNotification',
+            'useLoadingBar'
+          ]
+        }
+      ]
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()]
+    })
+  ]
+})
