4d5d0e3996ccd2bf4b0fe87327b0f47ea46801d6
LazuliKao <lazulikao@nbt.edu.cn>
2022-12-17 11:51:20 +0800

    Update FormatPages.vue

--- config/PFLP.ConfigEditor.Vue/src/components/FormatPages.vue
+++ config/PFLP.ConfigEditor.Vue/src/components/FormatPages.vue
@@ -5,7 +5,25 @@
         </template>
         <template #default="{ value }">
             <div style="display: flex; align-items: center; width: 100%">
-                <n-input v-model:value="value.string" :type="(textarea ? 'textarea' : 'text')" :on-change="onUpdate" />
+                <div v-if="textarea" style="display: flex; align-items: center; width: 100%">
+                    <n-switch v-model:value="value.multiline" size="large">
+                        <template #checked>
+                            多行
+                        </template>
+                        <template #unchecked>
+                            单框
+                        </template>
+                    </n-switch>
+                    <n-input v-if="value.multiline" v-model:value="value.string"
+                        :type="(textarea ? 'textarea' : 'text')" :on-change="onUpdate" />
+                    <n-dynamic-input v-model:value="value.s" :on-create="onCreate" :on-update-value="onUpdate">
+
+                    </n-dynamic-input>
+                </div>
+                <div v-else>
+                    <n-input v-model:value="value.string" :type="(textarea ? 'textarea' : 'text')"
+                        :on-change="onUpdate" />
+                </div>
             </div>
         </template>
     </n-dynamic-input>
@@ -13,6 +31,7 @@
 </template>
 <script lang="ts">
 import { ref } from 'vue'
+import { computed } from 'vue'
 export default {
     props: {
         data: {
@@ -51,6 +70,14 @@ export default {
                 }
                 return item.string
             }))
+        },
+        calcString: function (item: string) {
+            return computed({
+                get: () => item,
+                set: (value) => {
+                    item = value
+                }
+            })
         }
     }
 }
