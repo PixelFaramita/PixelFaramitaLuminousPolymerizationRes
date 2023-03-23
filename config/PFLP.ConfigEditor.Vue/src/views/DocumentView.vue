b73b59f73597d151351cd213a622be3f6a89b3e5
LazuliKao <lazulikao@nbt.edu.cn>
2023-03-23 17:52:36 +0800

    sync

--- config/PFLP.ConfigEditor.Vue/src/views/DocumentView.vue
+++ config/PFLP.ConfigEditor.Vue/src/views/DocumentView.vue
@@ -1,26 +1,27 @@
 <template>
   <!-- <link rel="stylesheet" href="/node_modules/highlight.js/styles/atom-one-dark-reasonable.css"> -->
-  <link rel="stylesheet" href="/node_modules/highlight.js/styles/vs2015.css">
+  <link rel="stylesheet" href="/node_modules/highlight.js/styles/vs2015.css" />
   <div>
     <div v-html="html"></div>
   </div>
 </template>
 <script lang="ts" setup>
-import hljs from 'highlight.js/lib/core'
+import hljs from "highlight.js/lib/core";
 </script>
 <script lang="ts">
-import { marked } from 'marked';
+import { marked } from "marked";
 export default {
   data() {
     return {
-      markdown: '# 加载中...',
-    }
-  }, computed: {
+      markdown: "# 加载中...",
+    };
+  },
+  computed: {
     html() {
       return marked(this.markdown, {
         highlight: (code, lang) => {
-          const language = hljs.getLanguage(lang) ? lang : 'plaintext';
-          const result = hljs.highlight(code, { language }).value
+          const language = hljs.getLanguage(lang) ? lang : "plaintext";
+          const result = hljs.highlight(code, { language }).value;
           //add copy button to code block
           // border
           // background
@@ -28,72 +29,77 @@ export default {
           let copyButton = `<button class="copy-button">复制</button>`;
           //active all copy-button
           setTimeout(() => {
-            let copyButtons = document.querySelectorAll('.copy-button');
+            let copyButtons = document.querySelectorAll(".copy-button");
             if ((copyButtons as any).hasAddCopyEvent == true) return;
             (copyButtons as any).hasAddCopyEvent = true;
             copyButtons.forEach((copyButton) => {
-              copyButton.addEventListener('click', (e) => {
-                let codeBlock = (e as any).target.parentNode.querySelector('code');
+              copyButton.addEventListener("click", (e) => {
+                let codeBlock = (e as any).target.parentNode.querySelector(
+                  "code"
+                );
                 let range = document.createRange();
                 range.selectNode(codeBlock);
                 window.getSelection()?.removeAllRanges();
                 window.getSelection()?.addRange(range);
-                document.execCommand('copy');
-             //   window.getSelection()?.removeAllRanges();
-                (e as any).target.innerText = '已复制';
+                document.execCommand("copy");
+                //   window.getSelection()?.removeAllRanges();
+                (e as any).target.innerText = "已复制";
                 setTimeout(() => {
-                  (e as any).target.innerText = '复制';
+                  (e as any).target.innerText = "复制";
                 }, 1000);
               });
             });
-          }, 1000); 
+          }, 1000);
           // code block
           let codeBlock = `<pre class="hljs"><code>${result}</code></pre>`;
           // wrapper
           let wrapper = `<div class="code-block-wrapper">${copyButton}${codeBlock}</div>`;
           return wrapper;
-        }
+        },
       });
-    }
-  }, mounted() {
+    },
+  },
+  mounted() {
     let mirrors = [
-      'https://raw.githubusercontent.com/LazuliKao/PixelFaramitaLuminousPolymerizationRes/main/docs/RemoteCall.md',
-      'https://jsd.cdn.zzko.cn/gh/LazuliKao/PixelFaramitaLuminousPolymerizationRes@main/docs/RemoteCall.md',
-      'https://cdn.jsdelivr.net/gh/LazuliKao/PixelFaramitaLuminousPolymerizationRes@main/docs/RemoteCall.md',
-      'https://raw.iqiq.io/LazuliKao/PixelFaramitaLuminousPolymerizationRes/main/docs/RemoteCall.md',
-      'https://raw.fastgit.org/LazuliKao/PixelFaramitaLuminousPolymerizationRes/main/docs/RemoteCall.md',
-      'https://raw.kgithub.com/LazuliKao/PixelFaramitaLuminousPolymerizationRes/main/docs/RemoteCall.md',
-      'https://raw.sevencdn.com/LazuliKao/PixelFaramitaLuminousPolymerizationRes/main/docs/RemoteCall.md',
-      'https://raw.githack.com/LazuliKao/PixelFaramitaLuminousPolymerizationRes/main/docs/RemoteCall.md',
-      'https://github.moeyy.xyz/https://raw.githubusercontent.com/LazuliKao/PixelFaramitaLuminousPolymerizationRes/main/docs/RemoteCall.md',
+      "https://raw.githubusercontent.com/LazuliKao/PixelFaramitaLuminousPolymerizationRes/main/docs/RemoteCall.md",
+      "https://jsd.cdn.zzko.cn/gh/LazuliKao/PixelFaramitaLuminousPolymerizationRes@main/docs/RemoteCall.md",
+      "https://cdn.jsdelivr.net/gh/LazuliKao/PixelFaramitaLuminousPolymerizationRes@main/docs/RemoteCall.md",
+      "https://raw.iqiq.io/LazuliKao/PixelFaramitaLuminousPolymerizationRes/main/docs/RemoteCall.md",
+      "https://raw.fastgit.org/LazuliKao/PixelFaramitaLuminousPolymerizationRes/main/docs/RemoteCall.md",
+      "https://raw.kgithub.com/LazuliKao/PixelFaramitaLuminousPolymerizationRes/main/docs/RemoteCall.md",
+      "https://raw.sevencdn.com/LazuliKao/PixelFaramitaLuminousPolymerizationRes/main/docs/RemoteCall.md",
+      "https://raw.githack.com/LazuliKao/PixelFaramitaLuminousPolymerizationRes/main/docs/RemoteCall.md",
+      "https://github.moeyy.xyz/https://raw.githubusercontent.com/LazuliKao/PixelFaramitaLuminousPolymerizationRes/main/docs/RemoteCall.md",
     ];
     let i = 0;
     let load = () => {
-      fetch(mirrors[i++]).then(res => {
-        if (res.ok) {
-          res.text().then(text => {
-            this.markdown = text;
-          });
-        } else {
+      fetch(mirrors[i++])
+        .then((res) => {
+          if (res.ok) {
+            res.text().then((text) => {
+              this.markdown = text;
+            });
+          } else {
+            if (i < mirrors.length) {
+              load();
+            } else {
+              this.markdown = "加载失败";
+            }
+          }
+        })
+        .catch(() => {
           if (i < mirrors.length) {
             load();
           } else {
-            this.markdown = '加载失败';
+            this.markdown = "加载失败";
           }
-        }
-      }).catch(() => {
-        if (i < mirrors.length) {
-          load();
-        } else {
-          this.markdown = '加载失败';
-        }
-      });
+        });
     };
     load();
-  }
-}
+  },
+};
 </script>
-<style >
+<style>
 details {
   border: 1px solid #e6e6e6;
   border-radius: 4px;
@@ -104,13 +110,13 @@ details {
 details summary {
   cursor: pointer;
   outline: none;
-} 
+}
 .code-block-wrapper {
   border: 1px solid #f7e926;
   border-radius: 4px;
-  padding:4px;
+  padding: 4px;
   margin-left: 10px;
-} 
+}
 /** style all table header with green background*/
 table th {
   background-color: #00696b;
@@ -120,11 +126,12 @@ table td {
   padding: 5px;
 }
 /** style all table cell with border*/
-table td, table th {
+table td,
+table th {
   border: 1px solid #e6e6e6;
 }
 /** style copy button with material design style*/
-.copy-button { 
+.copy-button {
   background-color: #005d38;
   border: none;
   color: white;
@@ -142,12 +149,11 @@ table td, table th {
   top: 1;
   right: 0;
   /**show in top layer */
-  z-index: 1; 
+  z-index: 1;
 }
 /** style of divider */
 hr {
   border: 2px solid #300002;
   margin: 10px 0;
 }
-
 </style>
--- config/PFLP.ConfigEditor.Vue/vite.config.ts
+++ config/PFLP.ConfigEditor.Vue/vite.config.ts
@@ -1,28 +1,28 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [
     vue(),
     AutoImport({
       imports: [
-        'vue',
+        "vue",
         {
-          'naive-ui': [
-            'useDialog',
-            'useMessage',
-            'useNotification',
-            'useLoadingBar'
-          ]
-        }
-      ]
+          "naive-ui": [
+            "useDialog",
+            "useMessage",
+            "useNotification",
+            "useLoadingBar",
+          ],
+        },
+      ],
     }),
     Components({
-      resolvers: [NaiveUiResolver()]
-    })
-  ]
-})
+      resolvers: [NaiveUiResolver()],
+    }),
+  ],
+});
