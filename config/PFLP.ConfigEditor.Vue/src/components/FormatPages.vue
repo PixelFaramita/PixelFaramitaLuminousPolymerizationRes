fab3a36b8bc345c43e2eab727ee7194548a46c1f
LazuliKao <lazulikao@nbt.edu.cn>
2023-06-24 21:14:36 +0800

    sync

--- config/PFLP.ConfigEditor.Vue/src/components/FormatPages.vue
+++ config/PFLP.ConfigEditor.Vue/src/components/FormatPages.vue
@@ -1,106 +1,134 @@
 <template>
-    <n-dynamic-input show-sort-button v-model:value="datax" :on-create="onCreate" :on-update-value="onUpdate">
-        <template #create-button-default>
-            写点啥
-        </template>
-        <template #default="{ value }: { value: LineItem }">
-            <div class="fullwidth">
-                <div v-if="textarea" class="fullwidth">
-                    <n-switch v-model:value="value.multiline" size="large" :on-update-value="value.switchChanged">
-                        <template #checked>
-                            多行
-                        </template>
-                        <template #unchecked>
-                            单框
-                        </template>
-                    </n-switch>
-                    <n-dynamic-input v-if="value.multiline" v-model:value="value.lines" :on-create="() => ''"
-                        :on-update-value="onUpdate">
-                    </n-dynamic-input>
-                    <n-input v-else v-model:value="value.string" type="textarea" :on-update-value="onUpdate" />
-                </div>
-                <div v-else class="fullwidth">
-                    <n-input v-model:value="value.string" type="text" :on-change="onUpdate" />
-                </div>
-            </div>
-        </template>
-    </n-dynamic-input>
-    <n-code :code="JSON.stringify(data, null, 2)" language="json" show-line-numbers />
-    <n-divider />
+  <n-dynamic-input
+    show-sort-button
+    v-model:value="datax"
+    :on-create="onCreate"
+    :on-update-value="onUpdate"
+  >
+    <template #create-button-default> 写点啥 </template>
+    <template #default="{ value }: { value: LineItem }">
+      <div class="fullwidth">
+        <div v-if="textarea" class="fullwidth">
+          <n-switch
+            v-model:value="value.multiline"
+            size="large"
+            :on-update-value="value.switchChanged"
+          >
+            <template #checked> 多行 </template>
+            <template #unchecked> 单框 </template>
+          </n-switch>
+          <n-dynamic-input
+            v-if="value.multiline"
+            v-model:value="value.lines"
+            :on-create="() => ''"
+            :on-update-value="onUpdate"
+          >
+          </n-dynamic-input>
+          <n-input
+            v-else
+            v-model:value="value.string"
+            type="textarea"
+            :on-update-value="onUpdate"
+          />
+        </div>
+        <div v-else class="fullwidth">
+          <n-input
+            v-model:value="value.string"
+            type="text"
+            :on-change="onUpdate"
+          />
+        </div>
+      </div>
+    </template>
+  </n-dynamic-input>
+  <n-code
+    :code="JSON.stringify(data, null, 2)"
+    language="json"
+    show-line-numbers
+  />
+  <n-divider />
 </template>
 <script lang="ts">
-import { ref } from 'vue'
-import { computed } from 'vue'
+import { ref } from "vue";
 class LineItem {
-    constructor(string: string) {
-        this.string = string
-        this.lines = string.split('\n')
+  constructor(string: string) {
+    this.string = string;
+    this.lines = string.split("\n");
+  }
+  string: string;
+  multiline: boolean = true;
+  lines: string[] = [];
+  switchChanged = () => {
+    if (this.multiline) {
+      this.lines.splice(0, this.lines.length, ...this.string.split("\n"));
+    } else {
+      this.string = this.lines.join("\n");
     }
-    string: string
-    multiline: boolean = true
-    lines: string[] = []
-    switchChanged = () => {
-        if (this.multiline) {
-            this.lines.splice(0, this.lines.length, ...this.string.split('\n'))
-        } else {
-            this.string = this.lines.join('\n')
-        }
+  };
+  GetResult = (singleLine: boolean) => {
+    if (singleLine) {
+      return this.string;
     }
-    GetResult = () => {
-        if (this.multiline) {
-            return this.lines.join('\n')
-        } else {
-            return this.string
-        }
+    if (this.multiline) {
+      return this.lines.join("\n");
+    } else {
+      return this.string;
     }
+  };
 }
 export default {
-    props: {
-        data: {
-            type: Array<string | Array<string>>,
-            required: true
-        },
-        textarea: {
-            type: Boolean,
-            default: false
-        }
+  props: {
+    data: {
+      type: Array<string | Array<string>>,
+      required: true,
     },
-    data: function () {
-        return {
-            datax: ref(this.data.map(item => {
-                if (typeof item === 'string') {
-                    return new LineItem(item)
-                }
-                return new LineItem(item.join(''))
-            }))
-        }
+    textarea: {
+      type: Boolean,
+      default: false,
     },
-    methods: {
-        onCreate: function () {
-            return new LineItem('')
-        },
-        onUpdate: function () {
-            this.$props.data.splice(0, this.$props.data.length, ...this.datax.map(item => {
-                let result = item.GetResult()
-                if (result.includes('\n')) {
-                    return result.split('\n')
-                }
-                return result
-            }))
-        }
-    }
-}
+  },
+  data: function () {
+    return {
+      datax: ref(
+        this.data.map((item) => {
+          if (typeof item === "string") {
+            return new LineItem(item);
+          }
+          return new LineItem(item.join(""));
+        })
+      ),
+    };
+  },
+  methods: {
+    onCreate: function () {
+      return new LineItem("");
+    },
+    onUpdate: function () {
+      this.$props.data.splice(
+        0,
+        this.$props.data.length,
+        ...this.datax.map((item) => {
+          let result = item.GetResult(!this.textarea);
+          if (result.includes("\n")) {
+            return result.split("\n");
+          }
+          return result;
+        })
+      );
+    },
+  },
+};
 </script>
 <style scoped>
 .n-input {
-    flex-wrap: nowrap;
-    word-wrap: unset;
-    overflow-wrap: unset;
+  flex-wrap: nowrap;
+  word-wrap: unset;
+  overflow-wrap: unset;
 }
 
 div.fullwidth {
-    width: 100%;
-    display: flex;
-    align-items: center;
+  width: 100%;
+  display: flex;
+  align-items: center;
 }
 </style>
--- config/PFLP.ConfigEditor.Vue/src/components/MessageConfig.vue
+++ config/PFLP.ConfigEditor.Vue/src/components/MessageConfig.vue
@@ -1,143 +1,160 @@
 <template>
+  <div>
     <div>
-        <div>
-            <n-h4 prefix="bar">
-                <h2>PopupInfo/悬浮信息</h2>
-                <n-space>
-                    启用模块
-                    <n-switch size="large" v-model:value="popup.ModuleEnabled" />
-                </n-space>
-                <div v-if="popup.ModuleEnabled">
-                    <n-space>
-                        切换间隔<n-input-number step="250" v-model:value="popup.RefreshInterval">
-                            <template #suffix>
-                                毫秒
-                            </template>
-                        </n-input-number>
-                    </n-space>
-                    <format-pages :data="popup.Pages" textarea />
-                </div>
-            </n-h4>
-        </div>
-        <div>
-            <n-h4 prefix="bar">
-                <h2>BelowName/玩家名</h2>
-                <n-space>
-                    启用模块
-                    <n-switch size="large" v-model:value="belowname.ModuleEnabled" />
-                </n-space>
-                <div v-if="belowname.ModuleEnabled">
-                    <n-space>
-                        切换间隔<n-input-number step="250" v-model:value="belowname.RefreshInterval">
-                            <template #suffix>
-                                毫秒
-                            </template>
-                        </n-input-number>
-                    </n-space>
-                    <format-pages :data="belowname.Pages" textarea />
-                </div>
-            </n-h4>
-        </div>
-        <div>
-            <n-h4 prefix="bar">
-                <h2>Sidebar/侧栏</h2>
- <n-space>
-                    启用模块
-                    <n-switch size="large" v-model:value="sidebar.ModuleEnabled" />
-                </n-space>
-                <div v-if="sidebar.ModuleEnabled">
-                    <n-space>
-                        切换间隔<n-input-number step="250" v-model:value="sidebar.RefreshInterval">
-                            <template #suffix>
-                                毫秒
-                            </template>
-                        </n-input-number>
-                    </n-space>
-                    <format-pages-with-title :data="sidebar.Pages" textarea />
-                </div>
-            </n-h4>
-        </div>
-        <div>
-            <n-h4 prefix="bar">
-                <h2>PlayerList/在线列表</h2>
-                <n-space>
-                    启用模块
-                    <n-switch size="large" v-model:value="playerlist.ModuleEnabled" />
-                </n-space>
-                <div v-if="playerlist.ModuleEnabled">
-                    <n-space>
-                        切换间隔<n-input-number step="250" v-model:value="playerlist.RefreshInterval">
-                            <template #suffix>
-                                毫秒
-                            </template>
-                        </n-input-number>
-                    </n-space>
-                    <format-pages-with-title :data="playerlist.Pages" textarea />
-                </div>
-            </n-h4>
-        </div>
-        <div>
-            <n-h4 prefix="bar">
-                <h2>DynamicMotd/动态服务器名</h2>
-                <n-space>
-                    启用模块
-                    <n-switch size="large" v-model:value="motd.ModuleEnabled" />
-                </n-space>
-                <div v-if="motd.ModuleEnabled">
-                    <n-space>
-                        切换间隔<n-input-number step="250" v-model:value="motd.RefreshInterval">
-                            <template #suffix>
-                                毫秒
-                            </template>
-                        </n-input-number>
-                    </n-space>
-                    <format-pages :data="motd.Pages" />
-                </div>
-            </n-h4>
-        </div>
-        <n-h2 prefix="bar">断开连接提示优化</n-h2>
+      <n-h4 prefix="bar">
+        <h2>PopupInfo/悬浮信息</h2>
         <n-space>
-            启用模块
-            <n-switch size="large" v-model:value="disconnect.ModuleEnabled" />
+          启用模块
+          <n-switch size="large" v-model:value="popup.ModuleEnabled" />
         </n-space>
-        <div v-if="disconnect.ModuleEnabled">
-            <n-space>
-                在控制台显示断开连接的信息
-                <n-switch size="large" v-model:value="disconnect.ShowDisconnectInConsole" />
-            </n-space>
-            <n-space>
-                强制允许的协议版本
-                <n-dynamic-input v-model:value="disconnect.ExtraAllowIncomingProtocolVersions" :on-update-value="() => {
+        <div v-if="popup.ModuleEnabled">
+          <n-space>
+            切换间隔<n-input-number
+              step="250"
+              v-model:value="popup.RefreshInterval"
+            >
+              <template #suffix> 毫秒 </template>
+            </n-input-number>
+          </n-space>
+          <format-pages :data="popup.Pages" textarea />
+        </div>
+      </n-h4>
+    </div>
+    <div>
+      <n-h4 prefix="bar">
+        <h2>BelowName/玩家名</h2>
+        <n-space>
+          启用模块
+          <n-switch size="large" v-model:value="belowname.ModuleEnabled" />
+        </n-space>
+        <div v-if="belowname.ModuleEnabled">
+          <n-space>
+            切换间隔<n-input-number
+              step="250"
+              v-model:value="belowname.RefreshInterval"
+            >
+              <template #suffix> 毫秒 </template>
+            </n-input-number>
+          </n-space>
+          <format-pages :data="belowname.Pages" textarea />
+        </div>
+      </n-h4>
+    </div>
+    <div>
+      <n-h4 prefix="bar">
+        <h2>Sidebar/侧栏</h2>
+        <n-space>
+          启用模块
+          <n-switch size="large" v-model:value="sidebar.ModuleEnabled" />
+        </n-space>
+        <div v-if="sidebar.ModuleEnabled">
+          <n-space>
+            切换间隔<n-input-number
+              step="250"
+              v-model:value="sidebar.RefreshInterval"
+            >
+              <template #suffix> 毫秒 </template>
+            </n-input-number>
+          </n-space>
+          <format-pages-with-title :data="sidebar.Pages" textarea />
+        </div>
+      </n-h4>
+    </div>
+    <div>
+      <n-h4 prefix="bar">
+        <h2>PlayerList/在线列表</h2>
+        <n-space>
+          启用模块
+          <n-switch size="large" v-model:value="playerlist.ModuleEnabled" />
+        </n-space>
+        <div v-if="playerlist.ModuleEnabled">
+          <n-space>
+            切换间隔<n-input-number
+              step="250"
+              v-model:value="playerlist.RefreshInterval"
+            >
+              <template #suffix> 毫秒 </template>
+            </n-input-number>
+          </n-space>
+          <format-pages-with-title :data="playerlist.Pages" textarea />
+        </div>
+      </n-h4>
+    </div>
+    <div>
+      <n-h4 prefix="bar">
+        <h2>DynamicMotd/动态服务器名</h2>
+        <n-space>
+          启用模块
+          <n-switch size="large" v-model:value="motd.ModuleEnabled" />
+        </n-space>
+        <div v-if="motd.ModuleEnabled">
+          <n-space>
+            切换间隔<n-input-number
+              step="250"
+              v-model:value="motd.RefreshInterval"
+            >
+              <template #suffix> 毫秒 </template>
+            </n-input-number>
+          </n-space>
+          <format-pages :data="motd.Pages" />
+        </div>
+      </n-h4>
+    </div>
+    <n-h2 prefix="bar">断开连接提示优化</n-h2>
+    <n-space>
+      启用模块
+      <n-switch size="large" v-model:value="disconnect.ModuleEnabled" />
+    </n-space>
+    <div v-if="disconnect.ModuleEnabled">
+      <n-space>
+        在控制台显示断开连接的信息
+        <n-switch
+          size="large"
+          v-model:value="disconnect.ShowDisconnectInConsole"
+        />
+      </n-space>
+      <n-space>
+        强制允许的协议版本
+        <n-dynamic-input
+          v-model:value="disconnect.ExtraAllowIncomingProtocolVersions"
+          :on-update-value="() => {
                     disconnect.ExtraAllowIncomingProtocolVersions.splice(0, disconnect.ExtraAllowIncomingProtocolVersions.length, ...disconnect.ExtraAllowIncomingProtocolVersions.map(x => {
                         if (x === '') return 0;
                         return Number((x as string).toString().replace(/[^0-9]/g, ''));
                     }) as never[]);
-                }" :on-create="() => 390">
-                </n-dynamic-input>
-            </n-space>
-            <div style="margin-left: 15px;">
-                <n-h5 prefix="bar" type="info">未通过XboxLive在线验证：
-                    <n-input v-model:value="disconnect.NotAuthenticated" />
-                </n-h5>
-                <n-h5 prefix="bar" type="info">不在白名单内：
-                    <n-input v-model:value="disconnect.NotInWhitelist" />
-                </n-h5>
-                <n-h5 prefix="bar" type="info">客户端版本过旧：
-                    <n-input v-model:value="disconnect.OldClient" />
-                </n-h5>
-                <n-h5 prefix="bar" type="info">客户端版本过新：
-                    <n-input v-model:value="disconnect.OldServer" />
-                </n-h5>
-                <n-h5 prefix="bar" type="info">版本具体信息：
-                    <n-input v-model:value="disconnect.DetailedInfo" />
-                </n-h5>
-                <n-h5 prefix="bar" type="info">当前账号已在其他设备登录：
-                    <n-input v-model:value="disconnect.AlreadyLoginIn" />
-                </n-h5>
-            </div>
-        </div>
-
+                }"
+          :on-create="() => 390"
+        >
+        </n-dynamic-input>
+      </n-space>
+      <div style="margin-left: 15px">
+        <n-h5 prefix="bar" type="info"
+          >未通过XboxLive在线验证：
+          <n-input v-model:value="disconnect.NotAuthenticated" />
+        </n-h5>
+        <n-h5 prefix="bar" type="info"
+          >不在白名单内：
+          <n-input v-model:value="disconnect.NotInWhitelist" />
+        </n-h5>
+        <n-h5 prefix="bar" type="info"
+          >客户端版本过旧：
+          <n-input v-model:value="disconnect.OldClient" />
+        </n-h5>
+        <n-h5 prefix="bar" type="info"
+          >客户端版本过新：
+          <n-input v-model:value="disconnect.OldServer" />
+        </n-h5>
+        <n-h5 prefix="bar" type="info"
+          >版本具体信息：
+          <n-input v-model:value="disconnect.DetailedInfo" />
+        </n-h5>
+        <n-h5 prefix="bar" type="info"
+          >当前账号已在其他设备登录：
+          <n-input v-model:value="disconnect.AlreadyLoginIn" />
+        </n-h5>
+      </div>
     </div>
+  </div>
 </template>
 <script lang="ts" setup>
 import FormatPages from "../components/FormatPages.vue";
@@ -147,25 +164,25 @@ import FormatPagesWithTitle from "../components/FormatPagesWithTitle.vue";
 import { useGlobalStore } from "../stores/global";
 const store = useGlobalStore();
 export default {
-    data() {
-        return {
-            popup: store.config.Popup,
-            belowname: store.config.BelowName,
-            playerlist: store.config.PlayerList,
-            sidebar: store.config.Sidebar,
-            motd: store.config.DynamicMotd,
-            disconnect: store.config.FriendlyDisconnect,
-        };
-    }
-}
+  data() {
+    return {
+      popup: store.config.Popup,
+      belowname: store.config.BelowName,
+      playerlist: store.config.PlayerList,
+      sidebar: store.config.Sidebar,
+      motd: store.config.DynamicMotd,
+      disconnect: store.config.FriendlyDisconnect,
+    };
+  },
+};
 </script>
 <style scoped>
 .n-space {
-    align-items: center;
-    margin-bottom: 10px;
+  align-items: center;
+  margin-bottom: 10px;
 }
 
 .n-divider {
-    margin: 3px;
+  margin: 3px;
 }
 </style>
